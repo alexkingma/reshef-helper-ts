1b832fc27cb0f04b7f058a7159fe50827f695bb2
Alex Kingma <alexkingma0@gmail.com>
2022-10-23 15:16:57 +1000

    add buttons to switch between opponent/cardlist views

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import CardList from "./components/CardList";
 
 import OpponentList from "./components/OpponentList";
 
 const App = () => {
-  return <OpponentList />;
+  const [mode, setMode] = useState("opponentlist");
+
+  return (
+    <>
+      <button onClick={() => setMode("cardlist")}>Show Card List</button>
+      <button onClick={() => setMode("opponentlist")}>
+        Show Opponent List
+      </button>
+      {mode === "opponentlist" ? <OpponentList /> : <CardList />};
+    </>
+  );
 };
 
 export default App;
