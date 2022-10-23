95ee224c9e4e358ddd4905a3cdb926467d5d21fe
Alex Kingma <alexkingma0@gmail.com>
2022-10-23 16:04:40 +1000

    add DC column to opponent table

--- src/components/OpponentList.tsx
+++ src/components/OpponentList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { default as opponents } from "../assets/opponents.json";
+import { cards } from "../assets/card_list.json";
 
 type CardName = keyof typeof opponents;
 
@@ -9,6 +10,15 @@ interface CardQuantityMap {
 }
 
 const OpponentList = () => {
+  const getBaseCost = (cardName: string): number => {
+    const card = cards.find((c) => c.name === cardName);
+    if (!card) {
+      console.log(`Card data for ${cardName} not found!`);
+      return -1;
+    }
+    return card.cost;
+  };
+
   return (
     <>
       <table>
@@ -16,6 +26,7 @@ const OpponentList = () => {
           <tr>
             <td>Opponent</td>
             <td>Cards</td>
+            <td>DC</td>
           </tr>
         </thead>
         <tbody>
@@ -26,6 +37,13 @@ const OpponentList = () => {
                 <td>{name}</td>
                 <td>
                   {Object.values(cardMap).reduce((total, x) => total + x, 0)}
+                </td>
+                <td>
+                  {Object.keys(cardMap).reduce(
+                    (total, cardName) =>
+                      total + getBaseCost(cardName) * cardMap[cardName],
+                    0
+                  )}
                 </td>
               </tr>
             );
