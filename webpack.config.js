bbc2be0556aaaa2ec84c2d61fa37c437afcd2f5b
Alex Kingma <alexkingma0@gmail.com>
2022-11-01 23:33:55 +1000

    import all images at once with require.context

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,11 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: "file-loader",
+        exclude: /node_modules/,
+      },
+      {
         test: /\.txt$/,
         type: "asset/source",
       },
